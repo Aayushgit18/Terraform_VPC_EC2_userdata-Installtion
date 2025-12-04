142b3e8433662745965bf457834b568e89f24003
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-04 18:04:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0"
   backend "s3" {
-    bucket  = "ayush-terraform-state"
+    bucket  = "ayush-terraform-state1245"
     key     = "infra/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
