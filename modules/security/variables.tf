b23a123f44746e760e41fb0542fb30daf857e574
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-04 18:13:28 +0530

    Update variables.tf

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,6 +1,23 @@
-variable "vpc_id" { type=string }
-variable "tags" { type=map(string) }
-variable "ssh_cidr" { type=string }
-variable "enable_http" { type=bool }
-variable "enable_https" { type=bool }
-variable "enable_jenkins" { type=bool }
+variable "vpc_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "ssh_cidr" {
+  type = string
+}
+
+variable "enable_http" {
+  type = bool
+}
+
+variable "enable_https" {
+  type = bool
+}
+
+variable "enable_jenkins" {
+  type = bool
+}
