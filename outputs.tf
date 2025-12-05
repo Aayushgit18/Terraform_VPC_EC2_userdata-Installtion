c513ebbc3775445a268ed0c46e381df6892fa049
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:01:31 +0530

    Add EC2 public IP output and format security group ID
    Added output for EC2 public IP and formatted security group ID output.

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
-output "vpc_id" { value = module.vpc.vpc_id }
-output "public_subnet_ids" { value = module.vpc.public_subnet_ids }
-output "private_subnet_ids" { value = module.vpc.private_subnet_ids }
-output "instance_ids" { value = module.ec2.instance_ids }
-output "instance_public_ips" { value = module.ec2.instance_public_ips }
-output "security_group_id" { value = module.security.sg_id }
+output "security_group_id" {
+  value = module.security.sg_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
