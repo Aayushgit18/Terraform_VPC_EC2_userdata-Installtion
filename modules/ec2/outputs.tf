ff3774e93e3438e2a5c934c3a45390797e33e67e
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:08:19 +0530

    Refactor output for instance public IPs

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,2 +1,3 @@
-output "instance_ids" { value=[for i in aws_instance.app: i.id] }
-output "instance_public_ips" { value=[for i in aws_instance.app: i.public_ip] }
+output "public_ips" {
+  value = aws_instance.app[*].public_ip
+}
