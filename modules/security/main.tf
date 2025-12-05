a9dd88e54fe6a5b50b832851141d2cf1fbceb131
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:07:49 +0530

    Refactor security group configuration for DevSecOps

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,36 +1,26 @@
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-security-group"
-  description = "Security group for EC2"
+resource "aws_security_group" "main" {
+  name        = "devsecops-sg"
+  description = "Security group for DevSecOps tools"
   vpc_id      = var.vpc_id
 
-  # Allow all required ports
-  dynamic "ingress" {
-    for_each = var.allowed_ports
-    content {
-      description = "Allow port ${ingress.value}"
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
+  tags = merge(var.tags, { Name = "devsecops-sg" })
+}
 
-  # SSH access (user-controlled)
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
-  }
+resource "aws_security_group_rule" "allow_inbound" {
+  count             = length(var.allowed_ports)
+  type              = "ingress"
+  from_port         = var.allowed_ports[count.index]
+  to_port           = var.allowed_ports[count.index]
+  protocol          = "tcp"
+  cidr_blocks       = [var.ssh_cidr]
+  security_group_id = aws_security_group.main.id
+}
 
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = var.tags
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
 }
