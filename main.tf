5af414d4e6a841eff6c06b25ffea05fe165638ee
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:06:16 +0530

    Refactor allowed_ports and update module parameters
    Formatted allowed_ports for better readability and added comments for each port.

--- main.tf
+++ main.tf
@@ -7,22 +7,32 @@ module "vpc" {
 }
 
 module "security" {
-  source       = "./modules/security"
+  source = "./modules/security"
+
   vpc_id       = module.vpc.vpc_id
-  tags         = var.tags
   ssh_cidr     = var.ssh_cidr
-  allowed_ports = [22, 80, 443, 8080, 8081, 8082, 9000]
+  tags         = var.tags
+
+  allowed_ports = [
+    22,    # SSH
+    80,    # HTTP
+    443,   # HTTPS
+    8080,  # Jenkins
+    8081,  # Artifactory UI
+    8082,  # Artifactory API
+    9000   # SonarQube
+  ]
 }
 
 module "ec2" {
-  source             = "./modules/ec2"
-  instance_count     = var.instance_count
-  ami                = var.ami
-  instance_type      = var.instance_type
-  key_name           = var.key_name
-  subnet_id          = module.vpc.public_subnet_ids[0]
-  sg_id              = module.security.sg_id
-  ebs_size           = var.ebs_size
+  source              = "./modules/ec2"
+  instance_count      = var.instance_count
+  ami                 = var.ami
+  instance_type       = var.instance_type
+  key_name            = var.key_name
+  subnet_id           = module.vpc.public_subnet_ids[0]
+  sg_id               = module.security.sg_id
+  ebs_size            = var.ebs_size
   associate_public_ip = var.associate_public_ip
-  tags               = var.tags
+  tags                = var.tags
 }
