8324a5b6d28d5c92c23a60643aa1a23a0c33d299
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 11:59:58 +0530

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "ec2_sg" {
-  name        = var.tags["Name"]
+  name        = "ec2-security-group"
   description = "Security group for EC2"
   vpc_id      = var.vpc_id
 
-  # Ingress ports
+  # Allow all required ports
   dynamic "ingress" {
     for_each = var.allowed_ports
     content {
-      description = "Allow inbound traffic"
+      description = "Allow port ${ingress.value}"
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
@@ -15,13 +15,21 @@ resource "aws_security_group" "ec2_sg" {
     }
   }
 
-  # Allow all egress
+  # SSH access (user-controlled)
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  # Allow all outbound traffic
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = var.tags
