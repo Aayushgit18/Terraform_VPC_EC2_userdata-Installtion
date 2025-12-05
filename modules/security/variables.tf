d02a64604d8a8abc1182f94c6b1d9fbb74eb8050
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:07:17 +0530

    Refactor security variables in variables.tf
    Removed type definitions for vpc_id, ssh_cidr, and tags variables. Added description to allowed_ports variable.

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,25 +1,9 @@
-variable "vpc_id" {
-  type = string
-}
-
-variable "ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0"
-}
+variable "vpc_id" {}
+variable "ssh_cidr" {}
+variable "tags" {}
 
 variable "allowed_ports" {
-  type = list(number)
-  default = [
-    22,    # SSH
-    80,    # HTTP
-    443,   # HTTPS
-    8080,  # Jenkins
-    8081,  # Artifactory UI
-    8082,  # Artifactory API
-    9000   # SonarQube
-  ]
-}
-
-variable "tags" {
-  type = map(string)
+  type        = list(number)
+  description = "Ports allowed inbound"
+  default     = [22, 80, 443]
 }
