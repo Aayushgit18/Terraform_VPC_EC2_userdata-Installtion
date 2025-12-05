ab171acf370de942ef2d44363edb4c4ac2b5549c
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:00:22 +0530

    Update variables.tf

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -2,12 +2,24 @@ variable "vpc_id" {
   type = string
 }
 
-variable "tags" {
-  type = map(string)
+variable "ssh_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
 }
 
-# List of allowed inbound ports
 variable "allowed_ports" {
-  type    = list(number)
-  default = [22, 80, 443, 8080, 8081, 8082, 9000]
+  type = list(number)
+  default = [
+    22,    # SSH
+    80,    # HTTP
+    443,   # HTTPS
+    8080,  # Jenkins
+    8081,  # Artifactory UI
+    8082,  # Artifactory API
+    9000   # SonarQube
+  ]
+}
+
+variable "tags" {
+  type = map(string)
 }
