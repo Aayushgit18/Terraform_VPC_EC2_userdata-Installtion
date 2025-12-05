5cf151160b0fc03901950f877bd144e8c5d92cc4
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-05 12:06:45 +0530

    Rename output 'ec2_public_ip' to 'ec2_public_ips'

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "security_group_id" {
   value = module.security.sg_id
 }
 
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
+output "ec2_public_ips" {
+  value = module.ec2.public_ips
 }
